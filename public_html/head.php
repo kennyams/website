74c9c57578390235c8f70d2fcc3d7c248c401f2d
ken <ken@pub.me.uk>
2021-02-12 12:51:39 +0000

    Adding favicon

--- public_html/head.php
+++ public_html/head.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"-->
 <link rel="stylesheet" href="/jquery-ui-1.12.1.custom/jquery-ui.css">
-<link rel="stylesheet"  href="/main.css"/>
+<!--link rel="stylesheet"  href="/main.css"/-->
 <link rel="stylesheet"  href="/header.css"/>
 <script src="//code.jquery.com/jquery-1.12.4.js"></script>
 <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
