b28f253d5913278754f4a218dca58afcc2e94544
Ken Williams <ken@pub.me.uk>
2021-02-23 17:10:47 +0000

    Adding files for IOT camera experiment

---
+++ public_html/projects/aws/camera.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>AWS Experiments</title>
+
+</head>
+<body>
+	<header>
+		<div id="right">
+			<h1>Experiments with AWS API</h1>
+		</div>
+	</header>
+<?php
+	$root=$_SERVER["DOCUMENT_ROOT"];
+	require("$root/../ssl/endpoints/iot.php");
+	require "$root/../phplib/aws/sigv4.php";
+	//error_log(IOT_ENDPOINT);
+	//error_log(IOT_PORT);
+	//error_log(IOT_CRED_FILE);
+  	echo "<textarea rows=\"50\" cols=\"180\">";
+	$move=False;
+	$pan=0;
+	$tilt=0;
+	$path="";
+	$payload="{";
+	if(array_key_exists('pan',$_GET)){
+		$pan=$_GET['pan'];
+		$path="/topics/sdk/pantilt";
+		$payload.="\"pan\":".$pan.",";
+		$move=True;
+	}
+	if(array_key_exists('tilt',$_GET)){
+		$tilt=$_GET['tilt'];
+		$path="/topics/sdk/pantilt";
+		$payload.="\"tilt\":".$tilt.",";
+		$move=True;
+	}
+	if(array_key_exists('click',$_GET)){
+		print("click\n");
+		$path="/topics/sdk/takePic";
+		$payload.="\"takePic\":1,";
+	}
+
+	if($move){
+	}
+	$payload[strlen($payload)-1]="}";
+	Send($path,$payload);
+	echo "</textarea>";
+?>
+</body>
+</html>
---
+++ public_html/projects/aws/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php
+			$root=$_SERVER["DOCUMENT_ROOT"];
+			include "$root/head.php";
+		?>
+		<title>AWS Experiments</title>
+		<script src="https://sdk.amazonaws.com/js/aws-sdk-2.632.0.min.js"></script>
+		<link href="aws.css" rel="stylesheet"/>
+	</head>
+	<body>
+		<?php
+			$root=$_SERVER["DOCUMENT_ROOT"];
+			include "$root/header.php";
+		?>
+		<main>
+			<div id="pan"></div>
+			<div id="tilt"></div>
+			<button id="snap" onclick="takePic()">click</button>
+			<img id="pic" src="https://iotpicbucket.s3.eu-west-2.amazonaws.com/pic.jpg?"+performance.now() style="width:100;image-orientation: from-image;" width="100%" />
+			<iframe id="vid" src="https://www.youtube.com/embed/rQw8OsW6o3c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<!--width="928" height="522"-->
+		</main>
+	<script>
+		$("#pagetitle").html("<p>Internet of things(IOT) demo</p>");
+		$("#tilt").slider({
+			orientation: "vertical",
+			min:-90,
+			max:90,
+			step:5,
+			change:function(event, ui){
+				console.log(ui.value);
+				var xhttp = new XMLHttpRequest();
+				xhttp.open("GET", "camera.php?tilt="+ui.value);
+				xhttp.send();
+				window.setTimeout(takePic,1000);
+				window.setTimeout(getStatus,300);
+			}
+		});
+		$("#pan").slider({
+			min:-90,
+			max:90,
+			step:5,
+			change:function(event, ui){
+				console.log(ui.value);
+				var xhttp = new XMLHttpRequest();
+				xhttp.open("GET", "camera.php?pan="+ui.value);
+				xhttp.send();
+				window.setTimeout(takePic,1000);
+				window.setTimeout(getStatus,300);
+			}
+		});
+		function loadPic(){
+			console.log("loadPic");
+			var pic = document.getElementById("pic").src="https://iotpicbucket.s3.eu-west-2.amazonaws.com/pic.jpg?"+performance.now();
+		}
+		function takePic(){
+			console.log("takePic");
+			var xhttp = new XMLHttpRequest();
+			xhttp.open("GET", "camera.php?click=1");
+			xhttp.send();
+			window.setTimeout(loadPic,3000);
+			window.setTimeout(getStatus,300);
+		}
+		function movev(event){
+			console.log("move");
+			var ang = event.currentTarget.value;
+			var id = event.currentTarget.id;
+			console.log(ang);
+			var xhttp = new XMLHttpRequest();
+			if(id=="pan"){
+				xhttp.open("GET", "camera.php?pan="+ang);
+			}else{
+				xhttp.open("GET", "camera.php?tilt="+ang);
+			}
+			xhttp.send();
+			window.setTimeout(takePic,1000);
+			window.setTimeout(getStatus,300);
+
+		}
+		function getStatus(){
+			var xhttp = new XMLHttpRequest();
+			xhttp.addEventListener("load",function(){
+				try{
+						var pos = JSON.parse(this.responseText);
+						console.log(pos.tilt);
+						console.log(pos.pan);
+						document.getElementById("tilt").value = pos.tilt;
+						document.getElementById("pan").value = pos.pan;
+					}catch{
+					}
+			});
+			try{
+				var res = xhttp.open("GET", "status.php");
+			}catch{
+			}
+			xhttp.send();
+		}
+
+		document.getElementById("pan").addEventListener("change",movev);
+		document.getElementById("tilt").addEventListener("change",movev);
+	</script>
+	</body>
+</html>
+
