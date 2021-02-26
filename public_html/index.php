83664f648f896bc54b566cdb95a377a0d56bdf19
Ken Williams <ken@pub.me.uk>
2021-02-26 12:38:15 +0000

    fix url to website on github

--- public_html/index.php
+++ public_html/index.php
@@ -14,7 +14,7 @@
 			include "$root/header.php";
 		?>
 		<main>
-			<a href="https://github.com/kennyams">Source code to this site can be found here</a>
+			<a href="https://github.com/kennyams/website">Source code to this site can be found here</a>
 		</main>
 	</body>
 </html>
