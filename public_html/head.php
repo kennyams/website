06080237e6973bb1d8f7e7306bf1ba4543f2b0a5
Ken Williams <ken@pub.me.uk>
2022-04-12 17:40:05 +0100

    update jquery

--- public_html/head.php
+++ public_html/head.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="/jquery-ui-1.12.1.custom/jquery-ui.css">
 <!--link rel="stylesheet"  href="/main.css"/-->
 <link rel="stylesheet"  href="/header_v1.css"/>
-<script src="//code.jquery.com/jquery-1.12.4.js"></script>
-<script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
 
 
